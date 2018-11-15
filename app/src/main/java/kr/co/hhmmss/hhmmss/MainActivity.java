369699393f1a086216850fb3249e1903a9c0b119
perext <perext5528@gmail.com>
2018-11-15 23:10:59 +0900

    [ADD] BottomNavigation, Toolbar

--- app/src/main/java/kr/co/hhmmss/hhmmss/MainActivity.java
+++ app/src/main/java/kr/co/hhmmss/hhmmss/MainActivity.java
@@ -1,13 +1,99 @@
 package kr.co.hhmmss.hhmmss;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Configuration;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bnv;
+    android.support.v7.widget.Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bnv = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        //Toolbar
+        toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.Toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp);
+
+
+
+        //actionBar.setDisplayShowTitleEnabled(false);
+
+        /*FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.add(R.id.frag_calendar, new CalendarActivity());
+        ft.commit();*/
+
+        //TODO Fragment or Activity // Visualbank benchmark
+
+
+        //BottomNavigation
+        bnv.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.Calendar:
+                        fragment = new CalendarActivity();
+                        return true;
+                    case R.id.TimeDiary:
+                        new TimeDiaryActivity();
+                        return true;
+                    case R.id.ToDo:
+                        new TodoActivity();
+                        return true;
+                }
+                return false;
+            }
+        });
     }
+
+
+    //Toolbar add icon click
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    //Toolbar add icon click
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.add:
+                Toast.makeText(getApplicationContext(), "add", Toast.LENGTH_SHORT);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
