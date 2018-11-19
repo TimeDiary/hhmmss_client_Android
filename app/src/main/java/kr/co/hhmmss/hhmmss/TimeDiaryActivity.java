3d8f20d0c6586c1b14cbec37729a130e76be1c38
Jho Lee <jooho_lee@outlook.kr>
2018-11-19 18:39:48 +0900

    [ADD] Add TimediaryManager to manage timediary.
    (Need test with UI.)

--- app/src/main/java/kr/co/hhmmss/hhmmss/TimeDiaryActivity.java
+++ app/src/main/java/kr/co/hhmmss/hhmmss/TimeDiaryActivity.java
@@ -2,18 +2,28 @@ package kr.co.hhmmss.hhmmss;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class TimeDiaryActivity extends Fragment {
-
+    public static final String TAG = TimeDiaryActivity.class.getSimpleName();
+    private FirebaseUser _user;
+    private String _uid;
+    private List<String> dateList;
 
 
     @Override
@@ -21,46 +31,109 @@ public class TimeDiaryActivity extends Fragment {
         return inflater.inflate(R.layout.frag_timediary, container, false);
     }
 
-
-    /* Timediary DB */
-    protected class TimediaryManager {
-        /* TODO: Add Javadoc */
-
-        // Current User
-        private FirebaseUser _user;
-
-        // Timediary DB Reference
-        private DatabaseReference timediaryDB;
-
-        // Rating for that date and time
-        private String _rating;
-
-        // Comment for that date and time
-        private String _comment;
+    @Override
+    public void onResume() {
+        super.onResume();
 
 
-        protected TimediaryManager(String uid) {
-            _user = FirebaseAuth.getInstance().getCurrentUser();
-            timediaryDB = FirebaseDatabase.getInstance().getReference("Timediary").child(uid);
+        /* */
+        _uid = _user.getUid();
+        TimediaryManager tdManager = new TimediaryManager(_uid);
+
+        tdManager.setmDateDiaryRef(dateList);
+
+
+        /* */
+
+    }
+
+
+}
+
+/* Timediary DB */
+class TimediaryManager {
+    /* TODO: Add Javadoc */
+
+    private static final String TAG = TimediaryManager.class.getSimpleName();
+    // Current User
+
+    // Timediary DB Reference
+    private DatabaseReference mTimediaryRef;
+    private List<DatabaseReference> mDateDiaryRefList;
+
+    TimediaryManager(String uid) {
+        mTimediaryRef = FirebaseDatabase.getInstance().getReference().child("Timediary").child(uid);
+
+    }
+
+    void setmDateDiaryRef(List<String> dateList) {
+
+        for (String date : dateList) {
+
+            mDateDiaryRefList.add(mTimediaryRef.child(date));
         }
 
-        protected void setRating(String rating) {
-            _rating = rating;
+        for (DatabaseReference mDateDiaryRef : mDateDiaryRefList) {
+
+            mDateDiaryRef.addValueEventListener(timediaryListener);
         }
 
-        protected void setComment(String comment) {
-            _comment = comment;
+
+    }
+
+    void writeTimediary(String date, String time, Timediary td) {
+        // TODO: Need test.
+        DatabaseReference tdRef = mTimediaryRef.child(date).child(time);
+        tdRef.setValue(td);
+
+    }
+
+    private ValueEventListener timediaryListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            // Get Post object and use the values to update the UI
+            Timediary timediary = dataSnapshot.getValue(Timediary.class);
+            // ...
+            Log.w(TAG, "loadTimediary:onDatachange : " + dataSnapshot.toString());
         }
 
-        private void save(String date, String time) {
-            // TODO: Need test.
-            DatabaseReference tdRef = timediaryDB.child(date).child(time);
-            tdRef.child("Rating").setValue(_rating.getClass());
-            tdRef.child("Comment").setValue(_comment.getClass());
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+            // Getting Post failed, log a message
+            Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+            // ...
+        }
+    };
+
+
+    /* */
+    protected class Timediary {
+        String rating;
+        String comment;
+
+        public Timediary() {
+            this.rating = null;
+            this.comment = null;
 
         }
 
-        // TODO: CRUD...
+        public Timediary(String comment) {
+            this.rating = null;
+            this.comment = comment;
+        }
+
+        public Timediary(String rating, String comment) {
+            this.rating = rating;
+            this.comment = comment;
+        }
+
+        public Map<String, Object> toMap() {
+            HashMap<String, Object> result = new HashMap<>();
+            result.put("rating", rating);
+            result.put("comment", comment);
+
+            return result;
+        }
 
     }
 }
