5389f65295cb41ee5072beaa1e5bf85c0fe323a2
Jho Lee <jooho_lee@outlook.kr>
2018-11-21 00:00:50 +0900

    [ADD] Add functions for manage Timediary DB.
    1) Add row into TimediaryFragment(test).
    2) Add TimediaryListenr.
    3) Add sample table for test into Timediary.

--- app/src/main/java/kr/co/hhmmss/hhmmss/TimediaryFragment.java
+++ app/src/main/java/kr/co/hhmmss/hhmmss/TimediaryFragment.java
@@ -1,21 +1,31 @@
 package kr.co.hhmmss.hhmmss;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.Exclude;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.Serializable;
 import java.util.HashMap;
@@ -29,65 +39,81 @@ public class TimediaryFragment extends Fragment {
     private String uid; // uid for current user.
 
     // view variables(for test)
-    private Button saveTdButton;
-    private TextView tdDateView;
-    private TextView tdTimeView;
-    private TextView tdRatingView;
-    private TextView tdCommentView;
+    View view;
+    private Spinner tdDateSpinner;
+    private TableLayout tdTableLayout;
+
     private final String TAG = TimediaryFragment.class.getSimpleName();
+
+
+    // date for one day view.(Sample)
+    private String _date;
+
 
     public TimediaryFragment() {
 
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanseState) {
-        View view = inflater.inflate(R.layout.frag_timediary, container, false);
+        view = inflater.inflate(R.layout.frag_timediary, container, false);
 
         // Get current user's uid
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         uid = firebaseUser.getUid();
 
         // Set reference of 'current user's timediary database'.
-        mTimediaryRef = FirebaseDatabase.getInstance().getReference().child("Timediary").child(uid);
+        mTimediaryRef = FirebaseDatabase.getInstance().getReference().child("timediary").child(uid);
+        tdDateSpinner = (Spinner) view.findViewById(R.id.tdDateSpinner);
+        tdTableLayout = (TableLayout) view.findViewById(R.id.tdTableLayout);
 
-        // Set view variables.
-        saveTdButton = (Button) view.findViewById(R.id.tdSaveButton);
-        tdDateView = (EditText) view.findViewById(R.id.tdDateDBView);
-        tdTimeView = (EditText) view.findViewById(R.id.tdTimeDBView);
-        tdRatingView = (EditText) view.findViewById(R.id.tdRatingDBView);
-        tdCommentView = (EditText) view.findViewById(R.id.tdCommentDBView);
 
-        // [START Set_OnClickListener_to_'SaveButton']
-        saveTdButton.setOnClickListener(buttonOnClickListener);
-        // [END Set_OnClickListener_to_'SaveButton']
+        // [START Setting_EventListener']
+        tdDateSpinner.setOnItemSelectedListener(tdItemSelectedListener);
+        // [END Setting_EventListener']
+
+        mTimediaryRef.orderByChild("time").addValueEventListener(timediaryListener);
 
 
         // Inflate the layout for this fragment
         return view;
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
 
+
+    // These will be saved to /timediary/<uid>/<key>/
     // [START write_Timediary_with_generatedKey]
     private void writeTimediary(String date, String time, String rating, String comment) {
-        String key = mTimediaryRef.push().getKey();
-        Log.i(TAG, "key: " + key);
 
         Timediary timediary = new Timediary(date, time, rating, comment);
-        Map<String, Object> timediaryValues = timediary.toMap();
 
-
-
-
-
-
-//        mTimediaryRef.updateChildren(childUpdates);
-
-        mTimediaryRef.push().setValue(timediary.toMap());
-
+        mTimediaryRef.push().setValue(timediary.toMap())
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        // Success.
+                        // TODO: Add reloading.
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Failed....
+                    }
+                });
 
     }
     // [END write_Timediary_with_generatedKey]
+
+    public void updateTimediary(String key, String value) {
+        // TODO: write this...
+    }
+
 
     /* [START] MyOnClickListener */
     Button.OnClickListener buttonOnClickListener = new Button.OnClickListener() {
@@ -95,89 +121,166 @@ public class TimediaryFragment extends Fragment {
         @Override
         public void onClick(View v) {
             switch (v.getId()) {
-                case R.id.tdSaveButton:
-                    String date = tdDateView.getText().toString();
-                    String time = tdTimeView.getText().toString();
-                    String rating = tdRatingView.getText().toString();
-                    String comment = tdCommentView.getText().toString();
-                    writeTimediary(date, time, rating, comment);
-                    break;
+//                case R.id.tdSaveButton:
+//                    String date = tdDateView.getText().toString();
+//                    String time = tdTimeView.getText().toString();
+//                    String rating = tdRatingView.getText().toString();
+//                    String comment = tdCommentView.getText().toString();
+//                    writeTimediary(date, time, rating, comment);
+//                    break;
                 default:
                     break;
             }
 
         }
     };
-
     /* [END] MyOnClickListener */
-    @IgnoreExtraProperties
-    public class Timediary implements Serializable {
 
-        public String _date;
-        public String _time;
-        public String _rating;
-        public String _comment;
+    /* [START TdItemSelectedListener */
+    Spinner.OnItemSelectedListener tdItemSelectedListener = new Spinner.OnItemSelectedListener() {
 
-
-        public Timediary() {
-            // Default constructor required for calls to DataSnapshot.getValue(Timediary.class)
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            _date = parent.getItemAtPosition(position).toString();
+            Log.d(TAG, "spinner item selected: (" + position + ", " + _date + ")");
 
         }
 
-        public Timediary(String date, String time, String rating, String comment) {
-            this._date = date;
-            this._time = time;
-            this._rating = rating;
-            this._comment = comment;
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+            Log.w(TAG, "spinner item selected: null");
+
         }
 
-        public void setDate(String date) {
-            this._date = date;
-        }
+    };
+    /* [END TdItemSelectedListener */
 
-        public String getDate() {
-            return this._date;
-        }
+    /* [START Push_TimediaryTableRow(sample). */
+    private void pushTableRowData(TableLayout timediaryTable, Timediary timediary) {
 
-        public void setTime(String time) {
-            this._time = time;
-        }
+        EditText datas[] = new EditText[3];
 
-        public String getTime() {
-            return this._time;
-        }
+        TableRow row = new TableRow(view.getContext());
 
-        public void setComment(String comment) {
-            this._comment = comment;
-        }
+        datas[0] = new EditText(view.getContext());
+        datas[1] = new EditText(view.getContext());
+        datas[2] = new EditText(view.getContext());
 
-        public String getComment() {
-            return _comment;
-        }
-
-        public void setRating(String rating) {
-            this._rating = rating;
-        }
-
-        public String getRating() {
-            return _rating;
+        for (EditText data : datas) {
+            data.setGravity(Gravity.CENTER);
+            row.addView(data);
         }
 
 
-        @Exclude
-        public Map<String, Object> toMap() {
-            HashMap<String, Object> result = new HashMap<>();
-            result.put("date", _date);
-            result.put("time", _time);
-            result.put("comment", _comment);
-            result.put("rating", _rating);
+        datas[0].setText(timediary.getTime());
+        datas[0].setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 3f));
+        datas[1].setText(timediary.getComment());
+        datas[1].setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 6f));
+        datas[2].setText(timediary.getRating());
+        datas[0].setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 2f));
 
 
-            return result;
-        }
+        timediaryTable.addView(row, 1);
 
 
     }
 
+    /* [END Push_TimediaryTableRow(sample). */
+
+
+    // [START TimediaryListener]
+    ValueEventListener timediaryListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                Timediary timediary = snapshot.getValue(Timediary.class);
+                Log.d(TAG, "loadTimediary:onDataChange -> " + timediary.toMap().toString());
+
+                pushTableRowData(tdTableLayout, timediary);
+
+
+            }
+        }
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError databaseError) {
+            Log.w(TAG, "loadPost:onCancelled", databaseError.toException());
+
+        }
+    };
+// [END TimediaryListener]
 
 }
+
+@IgnoreExtraProperties
+class Timediary implements Serializable {
+
+    public String date;
+    public String time;
+    public String rating;
+    public String comment;
+
+
+    public Timediary() {
+        // Default constructor required for calls to DataSnapshot.getValue(Timediary.class)
+
+    }
+
+    public Timediary(String date, String time, String rating, String comment) {
+        this.date = date;
+        this.time = time;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return this.date;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getTime() {
+        return this.time;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+
+    @Exclude
+    public Map<String, Object> toMap() {
+        HashMap<String, Object> result = new HashMap<>();
+        result.put("date", date);
+        result.put("time", time);
+        result.put("comment", comment);
+        result.put("rating", rating);
+
+
+        return result;
+    }
+
+
+}
+
+
+
