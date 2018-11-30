13b651926a008aec7f1e80e9528825475d254a96
Jho Lee <jooho_lee@outlook.kr>
2018-11-30 20:10:12 +0900

    [ADD] Add example layout for TimediaryDayDialogFragment

--- app/src/main/java/kr/co/hhmmss/hhmmss/TimediaryFragment.java
+++ app/src/main/java/kr/co/hhmmss/hhmmss/TimediaryFragment.java
@@ -13,8 +13,6 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Spinner;
@@ -140,27 +138,27 @@ public class TimediaryFragment extends Fragment {
     // [END onCreateView]
 
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds itmes to the action bar if it is present.
-        getActivity().getMenuInflater().inflate(R.menu.menu_timediary, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds itmes to the action bar if it is present.
+//        getActivity().getMenuInflater().inflate(R.menu.menu_timediary, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+//
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -193,16 +191,16 @@ public class TimediaryFragment extends Fragment {
             // Show 3 total pages.
             return NUM_PAGES;
         }
-
-        @Override
-        public CharSequence getPageTitle(int position) {
-            return "TimediaryTab " + (position + 1);
-        }
-
-        public void onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds itmes to the action bar if it is present.
-            getActivity().getMenuInflater().inflate(R.menu.menu_timediary, menu);
-        }
+//
+//        @Override
+//        public CharSequence getPageTitle(int position) {
+//            return "TimediaryTab " + (position + 1);
+//        }
+//
+//        public void onCreateOptionsMenu(Menu menu) {
+//            // Inflate the menu; this adds itmes to the action bar if it is present.
+//            getActivity().getMenuInflater().inflate(R.menu.menu_timediary, menu);
+//        }
     }
     /* .. */
 
