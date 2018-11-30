45d75bf3d431a67c5940028199e1410ab5a32e18
Jho Lee <jooho_lee@outlook.kr>
2018-11-30 23:56:07 +0900

    [ADD] Add Timediary Model.

---
+++ app/src/main/java/kr/co/hhmmss/hhmmss/timediary/TimediaryDoc.java
@@ -0,0 +1,78 @@
+package kr.co.hhmmss.hhmmss.timediary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TimediaryDoc {
+
+    private String uid;
+    private String date;
+    private String time;
+    private String comment;
+    private Float rating;
+
+    public TimediaryDoc() {
+    }
+
+    public TimediaryDoc(String uid, String date, String time, String comment, Float rating) {
+        this.uid = uid;
+        this.date = date;
+        this.time = time;
+        this.comment = comment;
+        this.rating = rating;
+
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+
+    public Map<String, Object> toMap() {
+        Map<String, Object> data = new HashMap<>();
+        data.put("uid", this.uid);
+        data.put("date", this.date);
+        data.put("time", this.time);
+        data.put("comment", this.comment);
+        data.put("rating", this.rating);
+
+        return data;
+
+    }
+}
---
+++ app/src/main/java/kr/co/hhmmss/hhmmss/timediary/TimediaryManager.java
@@ -0,0 +1,155 @@
+package kr.co.hhmmss.hhmmss.timediary;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class TimediaryManager {
+
+    private final String TAG = TimediaryFragment.class.getSimpleName();
+
+    // Google Firebase
+    @Nonnull
+    private FirebaseUser user;
+    @Nonnull
+    private String uID;
+
+    // Cloud Firestore
+    private FirebaseFirestore dbRoot;
+    private final CollectionReference timediaryCollectionRef;
+
+    public TimediaryManager() {
+        // [START set_Firebase]
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        uID = user.getUid();
+        // [END set_Firebase]
+
+        // [START set_CloudFirestore]
+        dbRoot = FirebaseFirestore.getInstance();
+        timediaryCollectionRef = dbRoot.collection("timediary");
+
+
+        // [END set_CloudFirestore]
+
+
+    }
+
+
+    public void add(TimediaryDoc doc) {
+        Map<String, Object> data = doc.toMap();
+
+        add(data);
+    }
+
+    public void add(Map<String, Object> data) {
+        timediaryCollectionRef
+                .add(data)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "TimediaryDocumentSnapshot written with ID: "
+                                + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding timediary document", e);
+                    }
+                });
+    }
+
+    public Map<String, TimediaryDoc> get(String date) {
+        final Map<String, TimediaryDoc> timediaryDocMap = new HashMap<>();
+
+        timediaryCollectionRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot doc : task.getResult()) {
+                                Log.d(TAG, doc.getId() + " => " + doc.getData());
+                                TimediaryDoc timediary = doc.toObject(TimediaryDoc.class);
+                                timediaryDocMap.put(timediary.getTime(), timediary);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting Timediary docs: ", task.getException());
+                        }
+                    }
+                });
+        return timediaryDocMap;
+    }
+
+    public TimediaryDoc get(String date, String time) {
+        final TimediaryDoc[] timediary = new TimediaryDoc[1];
+        timediaryCollectionRef.get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (DocumentSnapshot doc : task.getResult()) {
+                                Log.d(TAG, doc.getId() + " => " + doc.getData());
+                                timediary[0] = doc.toObject(TimediaryDoc.class);
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting Timediary docs: ", task.getException());
+                        }
+                    }
+                });
+        return timediary[0];
+    }
+
+    public void addRealTimeUpdate() {
+        // TODO: NOt completed....
+        // [START add_LocalChangeEventListener]
+        timediaryCollectionRef
+                .whereEqualTo("uid", uID)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "Listen failed.", e);
+                            return;
+                        }
+
+                        List<String> days = new ArrayList<>();
+                        for (QueryDocumentSnapshot doc : value) {
+                            if (doc.get("date") != null) {
+                                days.add(doc.getString("date"));
+                            }
+                        }
+                        Log.d(TAG, "Current dates in currentUser: " + days);
+                    }
+                });
+
+        // [END add_LocalChangeEventListener]
+    }
+
+    public void delete(final String date, final String time) {
+
+        // TODO
+    }
+}
