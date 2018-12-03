5bfaf46c0faf2b6b1d5afefb80fd1cd3a8edcc3d
Jho Lee <jooho_lee@outlook.kr>
2018-12-03 19:26:20 +0900

    [MOD] Failed.

--- app/src/main/java/kr/co/hhmmss/hhmmss/timediary/day/TimediaryDocListAdapter.java
+++ app/src/main/java/kr/co/hhmmss/hhmmss/timediary/day/TimediaryDocListAdapter.java
@@ -1,5 +1,7 @@
 package kr.co.hhmmss.hhmmss.timediary.day;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.Gravity;
@@ -8,21 +10,48 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import kr.co.hhmmss.hhmmss.R;
 import kr.co.hhmmss.hhmmss.timediary.TimediaryDoc;
+import kr.co.hhmmss.hhmmss.timediary.TimediaryFragment;
 
-public class TimediaryDocListAdapter extends RecyclerView.Adapter<TimediaryDocListAdapter.TimediaryDocViewHolder> {
-    private List<TimediaryDoc> timediaryDocs;
+public class TimediaryDocListAdapter extends RecyclerView.Adapter<TimediaryDocListAdapter.ViewHolder> {
+    private List<TimediaryDoc> timediaryDocList;
+    private Context context;
+    private FirebaseFirestore firestoreDB;
     private View timediaryDayView;
 
     private OnItemClickListener listener;
+
+
+    public TimediaryDocListAdapter(List<TimediaryDoc> timediaryDocs) {
+        this.timediaryDocList = timediaryDocs;
+
+    }
+
+    // [START ..]
+    private TimediaryDocListAdapter(List<TimediaryDoc> timediaryDocList, Context context, FirebaseFirestore firestoreDB) {
+        this.timediaryDocList = timediaryDocList;
+        this.context = context;
+        this.firestoreDB = firestoreDB;
+    }
+
+    // [END ..]
 
     // [START define_the_listener_interface]
     public interface OnItemClickListener {
         void onItemClick(View itemView, int position);
 
+    }
+
+    public interface OnTimediaryDaySelectedListener {
+        void onTimediaryDaySelected(DocumentSnapshot snapshot);
     }
     // [END define_the_listener_interface]
 
@@ -34,62 +63,27 @@ public class TimediaryDocListAdapter extends RecyclerView.Adapter<TimediaryDocLi
     // [END define_the_method_that_allows_the_parent_activity_or_fragment_to_define_the_listener]
 
 
-    public TimediaryDocListAdapter(List<TimediaryDoc> timediaryDocs) {
-        this.timediaryDocs = timediaryDocs;
-
-    }
-
-    public class TimediaryDocViewHolder extends RecyclerView.ViewHolder {
-
-        protected TextView timeView;
-        protected TextView commentView;
-        protected TextView ratingView;
-
-        TimediaryDocViewHolder(View view) {
-            super(view);
-            System.out.println("New TimediaryDocViewHolder");
-
-            this.timeView = view.findViewById(R.id.textview_recyclerview_time);
-            this.commentView = view.findViewById(R.id.textview_recyclerview_comment);
-            this.ratingView = view.findViewById(R.id.textview_recyclerview_rating);
-
-            view.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Triggers click upwards to the adapter on click
-                    if (listener != null) {
-                        int position = getAdapterPosition();
-                        if (position != RecyclerView.NO_POSITION) {
-                            listener.onItemClick(itemView, position);
-                        }
-                    }
-                }
-            });
-        }
-    }
-
-
     @NonNull
     @Override
-    public TimediaryDocViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         System.out.println("onCreateViewHolder");
 
 
         timediaryDayView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_timediary_day, parent, false);
 
-        return new TimediaryDocViewHolder(timediaryDayView);
+        return new ViewHolder(timediaryDayView);
     }
 
 
     @Override
-    public void onBindViewHolder(@NonNull TimediaryDocViewHolder viewHolder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
 
-        TimediaryDoc timediaryDoc = timediaryDocs.get(position);
+        final TimediaryDoc timediaryDoc = timediaryDocList.get(position);
 
         // [START set_views_into_list]
         // DateView
         System.out.println(timediaryDoc.getDate());
-//        TextView dateView = viewHolder.dateView;
+//        TextView dateView = ViewHolder.dateView;
 //        dateView.setText(timediaryDoc.getDate());
 //        dateView.setGravity(Gravity.CENTER);
         // TimeView
@@ -113,12 +107,67 @@ public class TimediaryDocListAdapter extends RecyclerView.Adapter<TimediaryDocLi
 
 
         //TODO
+
     }
 
     @Override
     public int getItemCount() {
-        return timediaryDocs.size();
+        return timediaryDocList.size();
     }
 
+    // [START ViewHolder]
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        @BindView(R.id.textview_recyclerview_time)
+        TextView timeView;
+
+        @BindView(R.id.textview_recyclerview_comment)
+        TextView commentView;
+
+        @BindView(R.id.textview_recyclerview_rating)
+        TextView ratingView;
+
+        ViewHolder(View view) {
+            super(view);
+            System.out.println("New ViewHolder");
+            ButterKnife.bind(this, itemView);
+
+
+        }
+//
+//        public void bind(final DocumentSnapshot snapshot,
+//                         final OnTimediaryDaySelectedListener listener) {
+//            TimediaryDoc timediaryDoc = snapshot.toObject(TimediaryDoc.class);
+//            Resources resources = itemView.getResources();
+//
+//            timeView.setText(timediaryDoc.getTime());
+//            commentView.setText(timediaryDoc.getComment());
+//            ratingView.setText(timediaryDoc.getRating().toString());
+//
+//            // Click listener
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    if (listener != null) {
+//                        listener.onTimediaryDaySelected(snapshot);
+//                    }
+//                }
+//            });
+//
+//
+//        }
+
+        private void updateTimediary(TimediaryDoc timediaryDoc) {
+            Intent intent = new Intent(context, TimediaryFragment.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra("UpdateTimediaryDate", timediaryDoc.getDate());
+            intent.putExtra("UpdateTimediaryTime", timediaryDoc.getTime());
+            intent.putExtra("UpdateTimediaryComment", timediaryDoc.getComment());
+            intent.putExtra("UpdateTimediaryRating", timediaryDoc.getRating());
+        }
+
+    }
+    // [END ViewHolder]
 
 }
